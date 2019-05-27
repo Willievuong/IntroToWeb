a85f9c9c43ce745296e94b272482f6185394d380
William Vuong <wpvuong@ucsd.edu>
2019-05-27 02:13:44 -0700

    Fixed axios tutorial, initialized future tutorial and changed folder structure

--- javascript/axios-tutorial/app.js
+++ javascript/axios-tutorial/app.js
@@ -1,7 +1,6 @@
 const axios = require('axios')
 const URL = 'Some URL'
 
-
 // Defining the default post headers
 axios.defaults.headers.post['Content-Type'] = 'application/JSON';
 
@@ -20,15 +19,23 @@ var getModel = () => {
   // Then the statement would be 
   // return axios.get('https://localhost/8000/' + 'profile/' + id )
   // Note that in this function the id is not passed in as a parameter, but it would be in the above statement 
-  return axios.get(URL)
+  axios.get(URL)
+  .then((response) =>{
+    console.log(response)
+    return response
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 }
 
 // When you would want to create a model, use axios.post(url, data)
-var postModel = () => {
+var postModel = (model) => {
   return axios.post(URL, model)
   // You would want to check if the request was actually made, a response will be given back from the server
   .then((response) => {
     console.log(response)
+    return response
   })
   // If there's any error 
   .catch((error) =>{
@@ -37,24 +44,26 @@ var postModel = () => {
 }
 
 // When you would want to update a model, use axios.put(url, data)
-var updateModel = () => {
+var updateModel = (model) => {
   return axios.put(URL, model)
   .then((response) => {
     console.log(response)
+    return response
   }).catch((error) =>{
     console.log(error)
   }) 
 }
 
 // Straight forward, if you would want to delete a model, use axios.deelte(url, data)
-var deleteModel = () => {
+var deleteModel = (model) => {
   return axios.delete(URL, model)
   .then((response) => {
     console.log(response)
+    return response
   }).catch((error) =>{
     console.log(error)
   }) 
 }
 
 // Lets run it! 
-console.log(getModel()) 
+getModel()
