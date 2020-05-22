418c297d0c7c2e1a43ebfae35a887e4cd1410225
willievuong <willievuong@gmail.com>
2020-05-22 10:49:42 -0700

    Adding more basic routes to introtoflask

--- python/introToFlask/server.py
+++ python/introToFlask/server.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def hello_world():
     return 'From one to America, how free are you tonight? ;)'
 
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+@app.route('/coolBeans', methods=['GET', 'POST'])
+def coolBeans():
+    if request.method == 'POST':
+        return "Beans"
+    else:
+        return "Cool"
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
